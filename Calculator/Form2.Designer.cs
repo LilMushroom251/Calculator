8252005044063f417aa0c36c256d18c9108299db
LilMushroom251 <2054052031minh@ou.edu.vn>
2022-10-22 15:11:59 +0700

    UTEST Bai1

---
+++ Calculator/Form2.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Calculator
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btTru = new System.Windows.Forms.Button();
+            this.btCong = new System.Windows.Forms.Button();
+            this.btNhan = new System.Windows.Forms.Button();
+            this.btChia = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtKQ);
+            this.groupBox1.Controls.Add(this.txtB);
+            this.groupBox1.Controls.Add(this.txtA);
+            this.groupBox1.Controls.Add(this.btNhan);
+            this.groupBox1.Controls.Add(this.btChia);
+            this.groupBox1.Controls.Add(this.btTru);
+            this.groupBox1.Controls.Add(this.btCong);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 31);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.groupBox1.Size = new System.Drawing.Size(617, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tính toán đơn giản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 31);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Kết quả";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 31);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Số b";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Số a";
+            // 
+            // btTru
+            // 
+            this.btTru.Location = new System.Drawing.Point(180, 223);
+            this.btTru.Name = "btTru";
+            this.btTru.Size = new System.Drawing.Size(101, 40);
+            this.btTru.TabIndex = 19;
+            this.btTru.Text = "Trừ";
+            this.btTru.UseVisualStyleBackColor = true;
+            this.btTru.Click += new System.EventHandler(this.btTru_Click);
+            // 
+            // btCong
+            // 
+            this.btCong.Location = new System.Drawing.Point(54, 223);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(101, 40);
+            this.btCong.TabIndex = 18;
+            this.btCong.Text = "Cộng";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
+            // 
+            // btNhan
+            // 
+            this.btNhan.Location = new System.Drawing.Point(316, 223);
+            this.btNhan.Name = "btNhan";
+            this.btNhan.Size = new System.Drawing.Size(101, 40);
+            this.btNhan.TabIndex = 21;
+            this.btNhan.Text = "Nhân";
+            this.btNhan.UseVisualStyleBackColor = true;
+            this.btNhan.Click += new System.EventHandler(this.btNhan_Click);
+            // 
+            // btChia
+            // 
+            this.btChia.Location = new System.Drawing.Point(446, 223);
+            this.btChia.Name = "btChia";
+            this.btChia.Size = new System.Drawing.Size(101, 40);
+            this.btChia.TabIndex = 20;
+            this.btChia.Text = "Chia";
+            this.btChia.UseVisualStyleBackColor = true;
+            this.btChia.Click += new System.EventHandler(this.btChia_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(198, 74);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(305, 38);
+            this.txtA.TabIndex = 22;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(198, 114);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(305, 38);
+            this.txtB.TabIndex = 23;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(198, 155);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(305, 38);
+            this.txtKQ.TabIndex = 24;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 357);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btNhan;
+        private System.Windows.Forms.Button btChia;
+        private System.Windows.Forms.Button btTru;
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+    }
+}
---
+++ Calculator/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btCong_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            Calculation c = new Calculation(a,b);
+            int kq = c.Execute("+");
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btTru_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            Calculation c = new Calculation(a, b);
+            int kq = c.Execute("-");
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btNhan_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            Calculation c = new Calculation(a, b);
+            int kq = c.Execute("*");
+            txtKQ.Text = kq.ToString();
+        }
+
+        private void btChia_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            Calculation c = new Calculation(a, b);
+            int kq = c.Execute("/");
+            txtKQ.Text = kq.ToString();
+        }
+    }
+}
